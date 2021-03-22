770801ac16f6d4dd97d3e8c456aa7fad18ee2e95
aranparrk <aranparrk@gmail.com>
2021-03-22 22:07:51 +0900

    JSP 게시판 만들기 11 - 게시판 글 목록 기능 구현하기

--- eclipse/BBS/src/bbs/BbsDAO.java
+++ eclipse/BBS/src/bbs/BbsDAO.java
@@ -1,6 +1,7 @@
 package bbs;
 
 import java.sql.*;
+import java.util.*;
 
 public class BbsDAO {
 	private Connection conn; // 데이터베이스에 접근해줄 수 있는 하나의 객체
@@ -20,7 +21,7 @@ public class BbsDAO {
 	}
 	
 	public String getDate() { // 현재의 시간을 가져오는 함수, 게시판의 글을 작성할 때 현재 서버의 시간을 넣어준다
-		String SQL = "select to_char(sysdate, 'yyyy/mm/dd') from dual"; //현재 날짜를 가져오는 쿼리문이다
+		String SQL = "SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD hh24:mi:ss') FROM DUAL"; //현재 날짜를 가져오는 쿼리문이다
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL); // 현재 연결되어 있는 객체를 이용해서 SQL문장을 실행준비 상태로 만들어준다
 			rs = pstmt.executeQuery(); // 실제 실행했을 때 나오는 결과를 가져온다
@@ -63,5 +64,44 @@ public class BbsDAO {
 			e.printStackTrace();
 		}
 		return -1; //데이터베이스 오류
+	}
+	
+	public ArrayList<Bbs> getList(int pageNumber) { // 특정한 리스트를 담아서 반환할 수 있게 만들어준다. 외부 라이브러리를 가져올 수 있게 임폴트 해준다 특정한 페이지에 따른 총 10개의 게시글을 만들 수 있게 해준다
+		String SQL = "SELECT * FROM (SELECT * FROM BBS WHERE bbsID < ? AND BBSAVAILABLE = 1 ORDER BY bbsID DESC) WHERE ROWNUM <= 10"; //게시글 번호는 1번부터 늘어나야하기 때문에 마지막에 쓰인 글을 가져와서 그 번호에 1을 더한 값이 그 다음 글이 된다 내림차순을해서 마지막으르 가져올 것
+		ArrayList<Bbs> list = new ArrayList<Bbs>(); //BBS를 담을 수 있는 리스트를 만들어 준다 
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL); // 현재 연결되어 있는 객체를 이용해서 SQL문장을 실행준비 상태로 만들어준다
+			pstmt.setInt(1, getNext() - (pageNumber - 1) * 10); // ?에 담길 내용, getNext는 그 다음에 작성될 글번호를 의미한다. 현재 게시글이 5개면 6이 나오기때문에 6보다 작은 값이 나오기 때문에 1~5까지 모두 나온다 특정한 페이지에서 10개만큼 게시글을 뽑아서 출력하기 위해서
+			rs = pstmt.executeQuery(); // 실제 실행했을 때 나오는 결과를 가져온다
+			while(rs.next()) { // 결과가 나올 때마다
+				Bbs bbs = new Bbs(); // 이 객체가 나오게 한다
+				bbs.setBbsID(rs.getInt(1)); //bbs에 담긴 모든 속성을 다 띄우기 때문에 각각 다 넣어주면 간단하게 작동한다
+				bbs.setBbsTitle(rs.getString(2)); 
+				bbs.setUserID(rs.getString(3)); 
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(6));
+				list.add(bbs); //리스트에 해당 인스턴스를 담아서 반환할 수 있도록 해주면 된다
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	//페이징처리를 위한 함수
+	public boolean nextPage(int pageNumber) { // 게시글이 10개 20개 10단위로 끝난다면 다음 페이지라는 버튼이 없어야 한다. 왜냐하면 다음페이지에는 다음글이 없기 때문이다
+		String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND	bbsAvailable = 1"; //게시글 번호는 1번부터 늘어나야하기 때문에 마지막에 쓰인 글을 가져와서 그 번호에 1을 더한 값이 그 다음 글이 된다 내림차순을해서 마지막ㅇ르 가져올 것	
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL); // 현재 연결되어 있는 객체를 이용해서 SQL문장을 실행준비 상태로 만들어준다
+			pstmt.setInt(1, getNext() - (pageNumber - 1) * 10); // ?에 담길 내용, getNext는 그 다음에 작성될 글번호를 의미한다. 현재 게시글이 5개면 6이 나오기때문에 6보다 작은 값이 나오기 때문에 1~5까지 모두 나온다 특정한 페이지에서 10개만큼 게시글을 뽑아서 출력하기 위해서
+			rs = pstmt.executeQuery(); // 실제 실행했을 때 나오는 결과를 가져온다
+			if(rs.next()) { // 결과가 하나라도 존재 한다면
+				return true; // 다음페이지로 넘어갈 수 있다
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
